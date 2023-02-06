10ea2a93267b34f481e3b3168759ba6b3fedc006
DADJOD <kamu57@mail.ru>
2023-02-06 21:26:15 +0300

    3

--- app/src/main/java/com/example/notessample/MainActivity.kt
+++ app/src/main/java/com/example/notessample/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.notessample
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mDb: SQLiteDatabase
 
     private lateinit var mAdapter: SimpleCursorAdapter
+    private var mNoteId = -1
+    private lateinit var mOldNote: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,37 +39,102 @@ class MainActivity : AppCompatActivity() {
         mAdapter = SimpleCursorAdapter(
             this, R.layout.note,
             null, from, to, 0)
-        mNotesList.adapter = mAdapter //!!
+        mNotesList.adapter = mAdapter
+        registerForContextMenu(mNotesList)
+        registerForContextMenu(mInputField)
     }
 
+    // redrawing
     override fun onResume() {
         super.onResume()
         showNotes()
     }
 
-    private fun showNotes() {
-        mDb = mHelper.writableDatabase //открывашка DataBase
-        val c = mDb.query(
-            DbOpenHelper.DB_TABLE, fields, null, null,
-            null, null, order
-        )
-        mAdapter.swapCursor(c)
-    }
-
+    // freeing up resources
     override fun onStop() {
         super.onStop()
         mDb.close()
     }
 
-    fun onOkButtonClick(view: View) {
-        val newNote = mInputField.text.toString().trim()
-        if (newNote.isNotEmpty()) {
+    private fun showNotes() {
+        mDb = mHelper.writableDatabase // open DataBase
+        val c = mDb.query(
+            DbOpenHelper.DB_TABLE, fields, null, null,
+            null, null, order
+        )
+        mAdapter.swapCursor(c) // show your items from list
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun onOkButtonClick(@Suppress("UNUSED_PARAMETER") view: View) {
+        val note = mInputField.text.toString().trim()
+
+        if (note.isNotEmpty()) {
             val values = ContentValues(1)
-            values.put(DbOpenHelper.COLUMN_NOTE, newNote)
-            mDb = mHelper.writableDatabase //открывашка DataBase
-            mDb.insert(DbOpenHelper.DB_TABLE, null, values)
+            values.put(DbOpenHelper.COLUMN_NOTE, note)
+            mDb = mHelper.writableDatabase // open DataBase
+
+            if (mNoteId >= 0) {
+                mDb.update(DbOpenHelper.DB_TABLE, values,
+                    "_id = $mNoteId", null)
+            } else {
+                mDb.insert(DbOpenHelper.DB_TABLE, null, values)
+            }
             showNotes()
         }
+        mNoteId = -1
+        mOldNote = null.toString()
         mInputField.text = null
+    }
+
+    // create context menu
+    override fun onCreateContextMenu(menu: ContextMenu?,
+                                     v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        when (v?.id) {
+            R.id.notesList ->
+                menuInflater.inflate(R.menu.notes_menu, menu)
+        }
+    }
+
+    // select item from context menu
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_edit -> {
+                val info: AdapterView.AdapterContextMenuInfo = // теперь через info можно получить item через .id. Через Adapter он имеет доступ к данным noteList
+                    item.menuInfo as AdapterView.AdapterContextMenuInfo
+                mOldNote = getNoteById(info.id).toString() // save for future use
+                mInputField.setText(mOldNote)              // fill in input field
+                mNoteId = info.id.toInt()                  // save for future use
+                true
+            }
+            R.id.item_delete -> {
+                val info: AdapterView.AdapterContextMenuInfo = // теперь через info можно получить item через .id. Через Adapter он имеет доступ к данным noteList
+                    item.menuInfo as AdapterView.AdapterContextMenuInfo
+                deleteNote(info.id)
+                showNotes()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    // return note by his id
+    private fun getNoteById(id: Long): String? {
+        val c = mDb.query(DbOpenHelper.DB_TABLE, fields,"_id = $id",
+            null, null, null, null)
+
+        var note: String? = null
+        if (c != null) {
+            c.moveToFirst()     // move Cursor to first
+            note = c.getString(
+                c.getColumnIndexOrThrow(DbOpenHelper.COLUMN_NOTE)) // get item which you choose in context menu
+            c.close()
+        }
+        return note
+    }
+
+    private fun deleteNote(id: Long) {
+        mDb.delete(DbOpenHelper.DB_TABLE, "_id = $id", null)
     }
 }
